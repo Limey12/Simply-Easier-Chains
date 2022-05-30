77bab1af7bcdde2d743a3af345e58a787110844a
Luca Pieples <llmd1@gmail.com>
2022-05-30 01:40:28 -0400

    first commit

---
+++ src/main/java/llmd1/simply/easier/chains/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package llmd1.simply.easier.chains.mixin;
+
+import llmd1.simply.easier.chains.SimplyEasierChains;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		SimplyEasierChains.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
